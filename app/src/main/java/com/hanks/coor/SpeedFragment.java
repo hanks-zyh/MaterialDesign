ed6b2d62485be9b04ba6aac2f5f20098aba08a5a
kiya <zjkkiya@163.com>
2015-11-13 08:39:31 +0800

    scroll

--- app/src/main/java/com/hanks/coor/SpeedFragment.java
+++ app/src/main/java/com/hanks/coor/SpeedFragment.java
@@ -3,14 +3,21 @@ package com.hanks.coor;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 /**
  * Created by kiya on 2015/11/13.
  */
 public class SpeedFragment extends Fragment {
+    private MyAdapter mAdapter;
+    private ArrayList dataList = new ArrayList();
+
     public static SpeedFragment newInstance() {
         Bundle args = new Bundle();
         SpeedFragment fragment = new SpeedFragment();
@@ -25,4 +32,33 @@ public class SpeedFragment extends Fragment {
         return view;
     }
 
+    @Override public void onViewCreated(View view, Bundle savedInstanceState) {
+        mAdapter = new MyAdapter();
+        RecyclerView recyclerView = (RecyclerView) getView().findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(mAdapter);
+    }
+
+    class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+        @Override public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list_speed, parent, false);
+            return new MyViewHolder(view);
+        }
+
+        @Override public void onBindViewHolder(MyViewHolder holder, int position) {
+
+        }
+
+        @Override public int getItemCount() {
+            return 100;
+        }
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
 }
