9e0d92b156f8f85d476109636ace1c2e84a09c11
hanks-zyh <zhangyuhan2014@gmail.com>
2015-11-14 15:52:31 +0800

    add FabAnimateActivity

---
+++ app/src/main/java/com/hanks/coor/FabAnimateActivity.kt
@@ -0,0 +1,14 @@
+package com.hanks.coor
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by kiya on 2015/11/14.
+ */
+class FabAnimateActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fabanimte)
+    }
+}
